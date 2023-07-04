88fee48581b0902850138aac78bdedb6b7557455
Jarod <jarsengkeoPro@gmail.com>
2023-07-04 15:14:59 +0200

    fixing

--- Assets/Scripts/FollowObject.cs
+++ Assets/Scripts/FollowObject.cs
@@ -8,18 +8,28 @@ public class FollowObject : MonoBehaviour
     [SerializeField]
     private bool verticalFollow = false;
     [SerializeField]
+    private bool useInFixedUpdate = false;
+    [SerializeField]
     private float smoothness = 5f;
 
-    void Start()
+
+    void LateUpdate()
     {
-        
+        if (!useInFixedUpdate)
+            Follow(Time.deltaTime);
     }
 
-    void Update()
+    private void FixedUpdate()
+    {
+        if (useInFixedUpdate)
+            Follow(Time.fixedDeltaTime);
+    }
+
+    private void Follow(float dt)
     {
         Vector3 p = target.transform.position;
         transform.position = Vector3.Lerp(transform.position,
             new Vector3(p.x, verticalFollow ? p.y : transform.position.y, p.z),
-            smoothness * Time.deltaTime);
+            smoothness * dt);
     }
 }
