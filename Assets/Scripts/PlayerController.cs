fc7a7b9d4d497c38ee45131dd879624f41f4c100
Jarod <jarsengkeoPro@gmail.com>
2023-07-04 17:32:30 +0200

    cleaning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
     private Rigidbody rb;
+    private Collider playerCollider;
     [SerializeField]
     private CameraController cameraController;
 
@@ -24,25 +26,49 @@ public class PlayerController : MonoBehaviour
     private float jumpForce = 5f;
 
     private Vector3 inputVelocity;
+    private bool bIsGrounded = false;
 
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        playerCollider = GetComponent<Collider>();
     }
 
     void Update()
     {
-        if (Input.GetButtonDown(jumpButton))
-            rb.AddForce(Vector3.up * jumpForce, useRigidbodyMass ? ForceMode.Impulse : ForceMode.VelocityChange);
+        CheckIsGrounded();
 
-        Vector3 movementDir = cameraController.cameraRight * Input.GetAxis(horizontalAxis) +
-            cameraController.cameraForward * Input.GetAxis(verticalAxis);
-        inputVelocity = movementDir * maxMovementSpeed;
+        JumpHandle();
+        HorizontalMovementHandle();
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector3(inputVelocity.x, rb.velocity.y, inputVelocity.z);
+    }
+
+    private void CheckIsGrounded()
+    {
+        bIsGrounded = true;
+    }
+
+    private void JumpHandle()
+    {
+        if (!bIsGrounded)
+            return;
+
+        if (Input.GetButtonDown(jumpButton))
+        {
+            rb.velocity -= Vector3.Scale(Vector3.up, rb.velocity);
+            rb.AddForce(Vector3.up * jumpForce, useRigidbodyMass ? ForceMode.Impulse : ForceMode.VelocityChange);
+        }
+    }
+
+    private void HorizontalMovementHandle()
+    {
+        Vector3 movementDir = cameraController.cameraRight * Input.GetAxis(horizontalAxis) +
+            cameraController.cameraForward * Input.GetAxis(verticalAxis);
+        inputVelocity = movementDir * maxMovementSpeed;
     }
 }
